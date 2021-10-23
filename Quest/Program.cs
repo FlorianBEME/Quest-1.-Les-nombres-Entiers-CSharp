280ef1fe48f537abbcac3f357c553d8ed15c6389
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-10-23 16:10:30 +0200

    Ajoutez des fichiers projet.

---
+++ Quest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            long b = long.MaxValue;
+            short c = short.MaxValue;
+
+            long result = a + b + c;
+        }
+    }
+}
