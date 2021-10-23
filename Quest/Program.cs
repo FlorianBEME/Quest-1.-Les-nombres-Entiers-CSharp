aa7e2d5cdfbbad9fbe8ab07809b5424169d04584
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-10-23 16:10:30 +0200

    Ajoutez des fichiers projet.

---
+++ Quest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint int_MaxValue = 2147483647;
+            ulong long_MaxValue = 9223372036854775808;
+            ushort short_MaxValue = 32768;
+
+            ulong result = int_MaxValue + long_MaxValue + short_MaxValue;
+        }
+    }
+}
